bebe449c81450d566614243a9b668334e6d23115
Madhur Nadamwar <112754729+madhurn29@users.noreply.github.com>
2023-05-06 16:10:49 +0530

    done

--- middleware/order.middleware.js
+++ middleware/order.middleware.js
@@ -3,14 +3,14 @@ const { OrderModel } = require("../model/order.model");
 
 async function order(req, res, next) {
   const token = req.headers.authorization;
-  var decoded = jwt.verify(token, "mock-11");
-
+  
   let bookInfo = req.body;
   req.body = {};
   req.body.books = bookInfo;
-
+  
   console.log(req.body);
   try {
+    var decoded = jwt.verify(token, "mock-11");
     console.log(decoded);
     if (decoded) {
       req.body.user = { name: decoded.name, email: decoded.email };
